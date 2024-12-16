ae3955d9c18477d2b91cc848dac431c98764a375
Josiren <egor.novikov666@bk.ru>
2024-12-16 20:01:17 +0300

    change: the app should be open not only in tg-mini-app

--- src/components/Root/ui/index.tsx
+++ src/components/Root/ui/index.tsx
@@ -2,30 +2,15 @@
 
 import React, { useEffect } from 'react';
 
-import { postEvent } from '@telegram-apps/sdk';
-import { SDKProvider } from '@telegram-apps/sdk-react';
 
 import '@/shared/ui/styles/globals.css';
 
 export const Root = ({ children }: { children: React.ReactNode }) => {
-  useEffect(() => {
-    const setupSDK = async () => {
-      try {
-        postEvent('web_app_expand');
-        postEvent('web_app_setup_swipe_behavior', { allow_vertical_swipe: false });
-        postEvent('web_app_set_header_color', { color: '#000000' });
-        postEvent('web_app_set_background_color', { color: '#000000' });
-      } catch (error) {
-        console.error('Wrong development place: go to your tg bot:', error);
-      }
-    };
-    setupSDK();
-  }, []);
   return (
-    <SDKProvider>
+
       <html lang="en">
         <body>{children}</body>
       </html>
-    </SDKProvider>
+
   );
 };
